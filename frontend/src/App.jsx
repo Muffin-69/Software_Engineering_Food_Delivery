e02827f9bc36c76649c8ba3347d927d263bc46ad
KateFedkova <kate.fedkova@gmail.com>
2026-05-05 22:20:11 +0300

    Update App routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import SignUp from "./pages/SignUpPage";
+import Login from "./pages/LoginPage";
+import Dashboard from "./pages/DashboardPage";
+import RestaurantPage from "./pages/RestaurantPage";
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/restaurants/:id"
+            element={
+              <ProtectedRoute>
+                <RestaurantPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
